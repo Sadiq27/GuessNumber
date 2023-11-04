0c502ec66c5275bd7de033a78c429c6302f2ac99
Sadiq <samedov.sadiq55@gmail.com>
2023-11-04 22:29:40 +0400

    Add sockets for server and client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+const string ip = "127.0.0.1";
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(ip);
+IPEndPoint endPoint = new IPEndPoint(address, port);
+
+Socket clientSocket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+await clientSocket.ConnectAsync(endPoint);
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+const string ip = "127.0.0.1";
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(ip);
+IPEndPoint endPoint = new IPEndPoint(address, port);
+
+Socket serverSocket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+serverSocket.Bind(endPoint);
+serverSocket.Listen(backlog: 5);
+
+serverSocket.Accept();
