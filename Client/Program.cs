b014f42c669fb6afdcbfcfe871e179002d064e27
Sadiq <samedov.sadiq55@gmail.com>
2023-11-09 00:15:59 +0400

    Add logic for server

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 const string ip = "127.0.0.1";
 const int port = 8080;
@@ -13,3 +14,28 @@ Socket clientSocket = new Socket(
     protocolType: ProtocolType.Tcp);
 
 await clientSocket.ConnectAsync(endPoint);
+
+try
+{
+    Console.WriteLine("Write message and press 'enter' to send...\n\n");
+    while (true)
+    {
+        string message = Console.ReadLine() ?? string.Empty;
+        var messageInBytes = Encoding.Unicode.GetBytes(message);
+
+        await clientSocket.SendAsync(messageInBytes);
+    }
+}
+catch (SocketException)
+{
+    Console.WriteLine("Disconnected from server!");
+    Environment.Exit(0);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("System error: " + ex);
+}
+finally
+{
+    clientSocket.Dispose();
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 const string ip = "127.0.0.1";
 const int port = 8080;
@@ -16,3 +17,60 @@ serverSocket.Bind(endPoint);
 serverSocket.Listen(backlog: 5);
 
 serverSocket.Accept();
+
+while (true)
+{
+    Socket clientSocket = await serverSocket.AcceptAsync();
+    Console.WriteLine($"Client was connected {clientSocket.RemoteEndPoint}");
+
+    int randomNum = new Random().Next(1, 101);
+    int attempts = 5;
+
+    byte[] buffer = new byte[1024];
+
+    ThreadPool.QueueUserWorkItem(async (socket) =>
+    {
+        try
+        {
+            while (attempts > 0)
+            {
+                var size = await socket.ReceiveAsync(buffer);
+                var requestMessage = Encoding.Unicode.GetString(buffer, 0, size);
+
+                int cliendNum;
+                if (int.TryParse(requestMessage, out cliendNum))
+                {
+                    if (cliendNum == randomNum)
+                    {
+                        string response = "Guessed right!";
+                        await socket.SendAsync(Encoding.Unicode.GetBytes(response));
+                        break;
+                    }
+                    else if (cliendNum < randomNum)
+                    {
+                        string response = "More";
+                        await socket.SendAsync(Encoding.Unicode.GetBytes(response));
+                    }
+                    else
+                    {
+                        string response = "Less";
+                        await socket.SendAsync(Encoding.Unicode.GetBytes(response));
+                    }
+                    attempts--;
+                }
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine($"Client {socket.RemoteEndPoint} disconnected!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"System error in {socket.RemoteEndPoint} client.\n{ex}");
+        }
+    }, clientSocket, false);
+
+    var gameOverMessage = "Game over. The hidden number was: " + randomNum;
+    var gameOverMessageInBytes = Encoding.Unicode.GetBytes(gameOverMessage);
+    await clientSocket.SendAsync(gameOverMessageInBytes);
+}
